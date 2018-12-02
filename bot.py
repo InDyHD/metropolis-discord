f03577c789bf78b463f50e4d03240ae46259b186
InDyHD <45535274+InDyHD@users.noreply.github.com>
2018-12-02 13:07:03 +0000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "NTE0MTQzNTgwMzAyODY4NTQ1.DuSY9Q.ig3tVqfPEUfVkWLuwY6s50UyH5M"
+
+client = commands.Bot(command_prefix = "_")
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name="MetropolisRP"))
+    print('Logado em:')
+    print(client.user.name)
+    print(client.user.id)
+    print('----------')
+    print("Bot Ready!")
+
+client.run(TOKEN)
