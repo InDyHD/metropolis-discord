cc8dd6045acb70e1b7f5f63313bbfa68593f9edf
InDyHD <45535274+InDyHD@users.noreply.github.com>
2019-02-11 21:31:10 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "NTE0MTQzNTgwMzAyODY4NTQ1.DuSY9Q.ig3tVqfPEUfVkWLuwY6s50UyH5M"
+TOKEN = "NTQ0NjI5ODMyMTU2NDQ2NzMw.D0N6JQ.QnBvXO-ibnSnyZkW7fVtSlfxaPk"
 
 client = commands.Bot(command_prefix = "_")
+
+@client.event
 
 @client.event
 async def on_ready():
